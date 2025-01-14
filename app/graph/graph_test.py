031605b9a9cc5298f99924634a28cb227673c573
Maye Maye Ahmed Saleck <purgan00@macbook.home>
2025-01-14 23:41:53 +0000

    Actualización en la configuración de Terraform Actions

--- app/graph/graph_test.py
+++ app/graph/graph_test.py
@@ -1,10 +1,8 @@
-import unittest
-import networkx as nx
+import pytest
 from graph.graph import Graph
 from graph.node import Node
 
-class TestGraph(unittest.TestCase):
-
+class TestGraph:
     def test_shortest_path(self):
         graph = Graph()
         graph.add_node("dog")
@@ -15,7 +13,7 @@ class TestGraph(unittest.TestCase):
 
         # Verifica el camino más corto entre "dog" y "cat"
         shortest_path = graph.shortest_path("dog", "cat")
-        self.assertEqual(shortest_path, ["dog", "dot", "cat"])
+        assert shortest_path == ["Node(dog)", "Node(dot)", "Node(cat)"]
 
     def test_clusters(self):
         graph = Graph()
@@ -23,12 +21,13 @@ class TestGraph(unittest.TestCase):
         graph.add_node("dot")
         graph.add_node("cat")
         graph.add_node("bat")
+
         graph.add_edge("dog", "dot")
         graph.add_edge("dot", "cat")
 
-        # Verifica los clústeres del grafo
+        # Debería haber 2 clústeres: uno con "dog", "dot", "cat" y otro con "bat"
         clusters = graph.clusters()
-        self.assertEqual(len(clusters), 2)  # Deberían haber 2 clústeres
+        assert len(clusters) == 2
 
     def test_high_connectivity_nodes(self):
         graph = Graph()
@@ -36,13 +35,14 @@ class TestGraph(unittest.TestCase):
         graph.add_node("dot")
         graph.add_node("cat")
         graph.add_node("bat")
+
         graph.add_edge("dog", "dot")
         graph.add_edge("dot", "cat")
         graph.add_edge("dot", "bat")
 
         # Busca nodos con al menos 2 conexiones
         high_connectivity_nodes = graph.high_connectivity_nodes(2)
-        self.assertIn(Node("dot"), high_connectivity_nodes)  # "dot" tiene 3 conexiones
+        assert Node("dot") in high_connectivity_nodes  # "dot" tiene 3 conexiones
 
     def test_all_paths(self):
         graph = Graph()
@@ -50,12 +50,13 @@ class TestGraph(unittest.TestCase):
         graph.add_node("dot")
         graph.add_node("cat")
         graph.add_node("bat")
+
         graph.add_edge("dog", "dot")
         graph.add_edge("dot", "cat")
 
         # Verifica que el único camino entre "dog" y "cat" es ["dog", "dot", "cat"]
         all_paths = graph.all_paths("dog", "cat", max_depth=3)
-        self.assertEqual(all_paths, [["dog", "dot", "cat"]])
+        assert all_paths == [["dog", "dot", "cat"]]
 
     def test_maximum_distance(self):
         graph = Graph()
@@ -63,13 +64,14 @@ class TestGraph(unittest.TestCase):
         graph.add_node("dot")
         graph.add_node("cat")
         graph.add_node("bat")
+
         graph.add_edge("dog", "dot")
         graph.add_edge("dot", "cat")
         graph.add_edge("dot", "bat")
 
         # Verifica la distancia máxima entre los nodos
         max_distance = graph.maximum_distance()
-        self.assertEqual(max_distance, 2)  # La distancia máxima es 2 entre "dog" y "cat" pasando por "dot"
+        assert max_distance == 2  # La distancia máxima es 2 entre "dog" y "cat" pasando por "dot"
 
     def test_nodes_by_degree(self):
         graph = Graph()
@@ -77,13 +79,14 @@ class TestGraph(unittest.TestCase):
         graph.add_node("dot")
         graph.add_node("cat")
         graph.add_node("bat")
+
         graph.add_edge("dog", "dot")
         graph.add_edge("dot", "cat")
         graph.add_edge("dot", "bat")
 
         # Verifica los nodos con grado 2 (en este caso, "dot")
         degree_2_nodes = graph.nodes_by_degree(2)
-        self.assertIn(Node("dot"), degree_2_nodes)  # "dot" tiene grado 3
+        assert Node("dot") in degree_2_nodes  # "dot" tiene grado 3
 
     def test_isolated_nodes(self):
         graph = Graph()
@@ -93,8 +96,5 @@ class TestGraph(unittest.TestCase):
 
         # "bat" es un nodo aislado porque no tiene conexiones
         isolated_nodes = graph.isolated_nodes()
-        self.assertIn(Node("bat"), isolated_nodes)  # "bat" está aislado
-        self.assertNotIn(Node("dog"), isolated_nodes)  # "dog" tiene una conexión
-
-if __name__ == '__main__':
-    unittest.main()
+        assert Node("bat") in isolated_nodes  # "bat" está aislado
+        assert Node("dog") not in isolated_nodes  # "dog" tiene una conexión
